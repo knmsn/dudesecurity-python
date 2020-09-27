8693d78436167c98d06d01f28b8d4562bfa2a21d
Gustavo <gustavoolrosa2019@gmail.com>
2020-09-26 23:52:53 -0500

    Update Setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-MIT License
+from setuptools import setup
 
-Copyright (c) 2018 Yan Orestes
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-
-- As it is a cybersecurity code, you will not be able to use it to violate the jurisdiction and laws of any country.
-- Leave the credits to the creator as inspiration for such an adaptation or update.
-
-
+setup(
+    name = 'dudesec',
+    version = '1.0.0',
+    author = 'Gustavo de Oliveira Rosa',
+    author_email = 'gustavoolrosa2019@gmail.com',
+    packages = ['dudesec'],
+    description = 'Um pacote de ferramentas para coders da área de segurança da informação.',
+    url = 'https://github.com/GustavoOliveiraRosa/DudSecPython'
+)
