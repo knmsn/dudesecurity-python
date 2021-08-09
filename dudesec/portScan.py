592fa8543b3a313945240851fced158c7078ad1d
Gustavo Rosa <50592755+KoenomatachiSan@users.noreply.github.com>
2021-08-08 22:14:22 -0300

    FIX import error

--- dudesec/portScan.py
+++ dudesec/portScan.py
@@ -12,21 +12,3 @@ import base64
 from random import choice
 import string
 
-def functionPS(x,y,debug):
-    portasAbertas = []
-    x = int(x)
-    y = int(y)
-    while True:
-        socket_verification = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        alvo = ('localhost', x)
-        resultado = socket_verification.connect_ex(alvo)
-        socket_verification.close()
-        if debug == True:
-            print(x)
-        if resultado == 0:
-            portasAbertas.append(int(x))
-        if x == y:
-            return portasAbertas
-            break
-        else:
-            x = x+1
